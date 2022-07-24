0268f5d7ae761180eb2cd68e688db7f3c3b12861
Hemratna Manohar Sonar <72693987+Hemratna-M-Sonar@users.noreply.github.com>
2022-07-24 19:32:18 +0530

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*kv25dje#dh01^yxy#m+msw(%l(m6=6=8b2sjp**b#-we9w(%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
